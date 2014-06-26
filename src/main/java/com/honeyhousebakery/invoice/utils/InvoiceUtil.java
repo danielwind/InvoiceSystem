5c5e7bc8bba1f2e4ae64cfe7b9bc3a1838103e77
Daniel Wind <Daniel.Wind@panerabread.com>
2014-06-25 19:25:47 -0600

    Invoice Updates to start with 240 (EXE)

--- src/main/java/com/honeyhousebakery/invoice/utils/InvoiceUtil.java
+++ src/main/java/com/honeyhousebakery/invoice/utils/InvoiceUtil.java
@@ -8,7 +8,7 @@ import java.util.logging.Logger;
  */
 public class InvoiceUtil {
     
-    private static int INVOICE_ID = 274;
+    private static int INVOICE_ID = 239;
     private static String LEADING_ZEROES = ""; 
     
     public static String getNextInvoiceID() {
